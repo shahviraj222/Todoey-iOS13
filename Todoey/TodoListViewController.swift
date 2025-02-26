91c8bba2bfea8e82e4594752afa3f86ec451ad96
shahviraj222 <viraj22feb@gmail.com>
2025-02-26 11:18:02 +0530

    table delegate and datasource is added

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//because we implemented the UITableViewController direclty we don't need to add the self.datadelegate and self.datasource.
 
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Add water to plant","Buy Milk","Go to Courier Centre"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+    }
+    
+//Mark Table View DataSource Methode
+    
+    //        no of rows we have in our table view
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
     }
 
-
+    
+    //      for setting each row in the table view.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell",for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+//Mark Table View Delegate Method
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+    }
+    
+    
 }
 
