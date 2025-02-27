47c47d177732b472391198257188bae910eb8d1e
shahviraj222 <viraj22feb@gmail.com>
2025-02-27 11:38:21 +0530

    the problem of task done mark is solved.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,41 +12,66 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Add water to plant","Buy Milk","Go to Courier Centre"]
+    var itemArray = [Item]()
+    
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view
+        
+        let newItem = Item()
+        newItem.title = "Buy Milk"
+        newItem.ismark = true
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Apple"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Buy Banana"
+        itemArray.append(newItem3)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item]{
+                    itemArray = items
+                }
     }
     
-//Mark Table View DataSource Methode
+    
+    //Mark Table View DataSource Methode
     
     //        no of rows we have in our table view
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-
+    
     
     //      for setting each row in the table view.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell",for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        itemArray[indexPath.row].ismark == true ? (cell.accessoryType = .checkmark) : (cell.accessoryType = .none)
+        
+//      cell.accessoryType = itemArray[indexPath.row].ismark ? .checkmark : .none
+        
         return cell
     }
     
-//Mark Table View Delegate Method
+    //Mark Table View Delegate Method
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(itemArray[indexPath.row])
+        //        print(itemArray[indexPath.row])
         
         //for selcting a particular row  tableView.cellForRow()
         
-        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].ismark = !itemArray[indexPath.row].ismark   //changing the ismark
+    
+        self.tableView.reloadData()
+        
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    // Mark Add item in todey list.
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var texFiled = UITextField()
@@ -54,20 +79,28 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new item in todey", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default){(action) in
-//            what will happen when user click on addITem button
-            print(texFiled.text)
+            
+            //what will happen when user click on addITem button
+            print(texFiled.text!)
+            
             if let a = texFiled.text{
-                self.itemArray.append(a)
+                let newItem = Item()
+                newItem.title = a
+                self.itemArray.append(newItem)
             }
+            
+            //saving the data in deafaults.
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+//            reloadData() call the datasource method again.
             self.tableView.reloadData()
-            print("Success!")
         }
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Add new item"
             texFiled = alertTextField
-            print(alertTextField.text) //nothing printed here
-          
+            print(alertTextField.text!) //nothing printed here
+            
         }
         
         alert.addAction(action)
---
+++ Todoey/model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Tododata.swift
+//  Todoey
+//
+//  Created by viraj shah on 27/02/25.
+//  Copyright © 2025 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title:String = ""
+    var ismark:Bool = false
+}
